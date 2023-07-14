6a91839e476669ad5da1ad70f53d252a95a56a81
Thom Pijnenburg <t.pijnenburg@elsevier.com>
2023-07-14 15:54:08 +0200

    sparse gcn layer implementation

--- lynks/layers.py
+++ lynks/layers.py
@@ -1,6 +1,8 @@
 import torch
 from torch.nn import Linear, Parameter
 import torch.nn.functional as F
+import torch.nn as nn
+
 from torch_geometric.nn import MessagePassing
 from torch_geometric.utils import add_self_loops, degree
 
@@ -62,3 +64,22 @@ class GCNConv(MessagePassing):
 
         # this is called by propagate method. Here we normalise the node features
         return norm.view(-1, 1) * x_j
+
+
+class SparseGCNLayer(nn.Module):
+    def __init__(self, c_in, c_out):
+        super().__init__()
+        self.projection = nn.Linear(c_in, c_out)
+
+    def forward(self, node_feats, adj_matrix):
+        """Inputs"""
+        num_neighbours = torch.sparse.sum(
+            adj_matrix, dim=-1).unsqueeze(-1).to_dense()
+        node_feats = self.projection(node_feats)
+
+        # node_feats = torch.sparse.mm(adj_matrix, node_feats)
+        node_feats = torch.mm(adj_matrix, node_feats)
+
+        # node_feats = node_feats.to_dense()
+        node_feats = torch.div(node_feats, num_neighbours)
+        return node_feats
